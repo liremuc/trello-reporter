469d2bed7f73c6706a0357242b83f764deee8b68
liremuc <your-github-email>
2025-01-01 15:28:15 +0100

    Update Flask app and Trello service

--- flask_app.py
+++ flask_app.py
@@ -1,20 +1,10 @@
 ﻿from flask import Flask, render_template, jsonify, request
-from trello_service import TrelloReportService
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/api/boards')
-def get_boards():
-    trello = TrelloReportService(
-        api_key='your_api_key',
-        token='your_token'
-    )
-    boards = trello.get_boards()
-    return jsonify(boards)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- trello_service.py
+++ trello_service.py
@@ -1,1 +1,11 @@
-﻿
+﻿class TrelloReportService:
+    def __init__(self, api_key=None, token=None):
+        self.api_key = api_key
+        self.token = token
+
+    def get_boards(self):
+        # Placeholder for now
+        return [
+            {'id': '1', 'name': 'Test Board 1'},
+            {'id': '2', 'name': 'Test Board 2'}
+        ]
